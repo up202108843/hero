192b30174e0413b65014eefaa43e2c0f36865260
Eduardo Oliveira <up202108843@up.pt>
2022-09-27 13:30:12 +0100

    Initial version

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,17 +1,31 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-public class Application {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        Terminal terminal = new
-                DefaultTerminalFactory().createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null); // we don't need a cursor
-        screen.startScreen(); // screens must be started
-        screen.doResizeIfNecessary(); // resize screen if necessary
+import java.io.IOException;
 
+public class Application {
+    public static void main(String[] args) throws IOException {
+        try {
+            Terminal terminal = new
+                    DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new
+                    DefaultTerminalFactory()
+                    .setInitialTerminalSize(terminalSize);
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
+                    [0]);
+            screen.refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
