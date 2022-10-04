5ab784afd56d52af123e958a9dafa072fc2e24b6
Eduardo José Neto Oliveira <up202108843@up.pt>
2022-10-04 09:28:28 +0100

    Initial version

---
+++ src/main/java/Game.java
@@ -0,0 +1,23 @@
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private Screen screen;
+    public Game(){
+        try {
+            Terminal terminal = new
+                DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen i
+            } catch (
+    IOException e) {
+        e.printStackTrace();
+        }
+    }
+}
